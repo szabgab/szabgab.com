0d0b29fafe18dc704c6b4f1329fe15bbb4af6861
Gabor Szabo <gabor@szabgab.com>
2024-03-01 12:53:10 +0200

    remove the unnecessary PSGI.pm

--- generate.pl
+++ generate.pl
@@ -165,4 +165,30 @@ sub create_sitemap {
     return $xml;
 }
 
+#if ($env->{PATH_INFO} =~ m{^/talks/perl_in_testing/(.*)}) {
+#    my $file = substr $1, 0, -5;
+#    if ($file =~ /^x\d+/) {
+#        return redirect( "https://code-maven.com/slides/test-automation-using-perl/" );
+#    }
+#    return redirect( "https://code-maven.com/slides/test-automation-using-perl/$file" );
+#}
+#if ($env->{PATH_INFO} =~ m{^/talks/fundamentals_of_perl/(.*)}) {
+#    my $file = substr $1, 0, -5;
+#    return redirect( "https://code-maven.com/slides/perl-programming/$file" );
+#}
+#return redirect("https://code-maven.com/raku") if $env->{PATH_INFO} =~ m{^/talks/perl6/};
+#return redirect("/$1") if $env->{PATH_INFO} =~ m{^/blog/.*/([^/]+)\.html$};
+
+
+#if ($env->{REQUEST_URI} =~ m{^/blog/.*/.*/([^/]+)\.html$}) {
+#    my $file = $1;
+#    if ($file =~ m{^\d+$}) {
+#        $file = Sz::Meta::ts_to_url($file);
+#    } else {
+#        $file .= ".html";
+#    }
+#    $file =~ s{^/+}{};
+#    return redirect( "$env->{'psgi.url_scheme'}://$env->{HTTP_HOST}/$file" );
+#}
+
 
